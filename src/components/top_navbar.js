920d3bc2750cd0e52d1c527b11ea2190247a61fb
prakashcrayond <prakash@crayond.co>
2020-10-22 18:12:00 +0530

    checkout page ui

--- src/components/top_navbar.js
+++ src/components/top_navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // {* -------------topnavbar  import  compoents -------------*}
-import { AppBar, Toolbar, IconButton, Typography, Button, Hidden, Drawer, ListItem, List, ListItemText, Container } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Button, Hidden, Drawer, ListItem, List, ListItemText } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 import MenuIcon from '@material-ui/icons/Menu';
